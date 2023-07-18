2f9e15e250abf4fbfd5455494e86173578ddc5f3
ananya150 <akblockchain15@gmail.com>
2023-07-18 15:09:25 +0530

    added responsivenes to header

--- src/components/homepage/header/index.tsx
+++ src/components/homepage/header/index.tsx
@@ -3,23 +3,24 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className='h-[7vh] flex mx-[20px] justify-between items-end'>
-        <Link href='#'>
-            <span className='text-white font-satoshi text-[18px]'>
+    <div className='h-[7vh] md:flex md:justify-between md:mx-[30px] items-end'>
+        <Link href='#' className='hidden md:block'>
+            <span className='text-white font-satoshi md:text-[18px]'>
                 © &nbsp; Ananya Khandelwal
             </span>
+            <div className='mt-[9px]'></div>
         </Link>
-        <div className='flex space-x-12 mr-8 '>
+        <div className='flex mx-auto md:mx-0 justify-center space-x-12 mr-8 pt-6'>
           <div className='group'>
-            <Link href='#'><span className='text-white font-satoshi text-[19px]'>Work</span></Link>
+            <Link href='#'><span className='text-white font-satoshi text-[18px] md:text-[19px]'>Work</span></Link>
             <div className='w-[5px] h-[5px] rounded-full bg-white mx-auto mt-1 opacity-0 group-hover:opacity-100'></div>
           </div>
           <div className='group'>
-            <Link href='#'><span className='text-white font-satoshi text-[19px]'>About</span></Link>
+            <Link href='#'><span className='text-white font-satoshi text-[18px] md:text-[19px]'>About</span></Link>
             <div className='w-[5px] h-[5px] rounded-full bg-white mx-auto mt-1 opacity-0 group-hover:opacity-100'></div>
           </div>
           <div className='group'>
-            <Link href='#'><span className='text-white font-satoshi text-[19px]'>Contact</span></Link>
+            <Link href='#'><span className='text-white font-satoshi text-[18px] md:text-[19px]'>Contact</span></Link>
             <div className='w-[5px] h-[5px] rounded-full bg-white mx-auto mt-1 opacity-0 group-hover:opacity-100'></div>
           </div>
         </div>
